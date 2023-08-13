a6eb006bae2316a8dd4c9b7fc940a64014e28554
Giiill <nadezhdinpro100@mail.ru>
2023-08-13 20:09:24 +0300

    uploaded new solutions

--- zalivDz.js
+++ zalivDz.js
@@ -17,20 +17,17 @@ result - [4,11,38]
 */
 
 // РЕШЕНИЕ
+// написание на reduce действительно отличный способ, ничего лучше не знаю что придумать
+// переписал на forEach но думаю что на reduce твой вариант все жё легче читается
 function searchIndexes(str, substr) {
-    let array = [];
-    let num = 0;
-    while (num != -1) {
-        num = str.indexOf(substr, num);
-        if (num != -1) {
-            array.push(num);
-            ++num;
-        };
-    };
-    return array;
-    
+    const replaceTo = str.replaceAll(substr, "~").split("");
+    let acc = [];
+    replaceTo.forEach((item, index, array) => {
+        return item === "~" ? acc.push(index + acc.length) : false;
+    });
+    return acc;
 }
-console.log(searchIndexes('далеко далеко пасутся кто? правильно - коровы', 'ко'));
+
 
 // ===============================================================================================
 
@@ -43,29 +40,31 @@ https://leetcode.com/problems/multiply-strings (https://leetcode.com/problems/mu
 “Error: can’t count as number”
 */
 // РЕШЕНИЕ
+// ===============================================================================================
+// данный код мой немного переписан, включил в него из твоего примера
+// замену запятых на точки, и он проходит все тесты. А твой код у меня наоборот не проходит все тесты
 var multiply = function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {
-        return 'Error: can\’t count as number';
-    };
-    if (num1.includes('.')) {
-        let n = num1.indexOf('.');
-        let numLength = num1.length - n - 1;
-        num1 = BigInt(Number(num1) * 10 ** numLength);
-        
-    };
-    if (num2.includes('.')) {
-        let n = num2.indexOf('.');
-        let numLength = num2.length - n - 1;
-        num2 = BigInt(Number(num2) * 10 ** numLength);
-        
-    };
-    
-    let result = BigInt(num1) * BigInt(num2);
-    
-    return String(result);
+    const parseNum1 = intoNumber(num1);
+    const parseNum2 = intoNumber(num2);
+
+   function intoNumber(num) {
+        let parseNum = num.replace(/[.]+|[,]+/g, ".");
+       if (isNaN(parseNum)) {
+           return 'Error: can\'t count as number';
+       };
+       if (parseNum.includes('.')) {
+           const lengthAfterPoint = parseNum.match(/\.(\d+)/)[1].length;
+           return parseNum = Number(parseNum) * 10 ** lengthAfterPoint;
+       } else {
+           return num;
+       };
+   }
+   return String(BigInt(parseNum1) * BigInt(parseNum2));
 };
 
-console.log(multiply("0.0000026", "3"));
+
+
+
 
 // ===============================================================================================
 
@@ -74,119 +73,116 @@ console.log(multiply("0.0000026", "3"));
 
 */
 // РЕШЕНИЕ
+// разобрал твой код и переписал его, можно сказать немного оптимизировал,
+// уменьшил кол-во шагов при вычислении символа. Например если символ меньше 10, то
+// вычислении начнётся с ключа 9, пропуская тем самым не нужные вычисления.
+// По идеи это должно ускорить выполнении функции.
 var intToRoman = function(num) {
-    const singleNumber = {
-        0: "",
-        1: "I",
-        2: "II",
-        3: "III",
-        4: "IV",
-        5: "V",
-        6: "VI",
-        7: "VII",
-        8: "VIII",
-        9: "IX"
+    const romanSigns = {
+        1000: "M", 
+        900: "CM", 
+        500: "D", 
+        400: "CD", 
+        100: "C", 
+        90: "XC", 
+        50: "L", 
+        40: "XL", 
+        10: "X", 
+        9: "IX", 
+        5: "V", 
+        4: "IV", 
+        1: "I", 
     };
-    const decimalNumbers = {
-        0: "",
-        1: "X",
-        2: "XX",
-        3: "XXX",
-        4: "XL",
-        5: "L",
-        6: "LX",
-        7: "LXX",
-        8: "LXXX",
-        9: "XC"
-    };
-    const hundrethsNumbers = {
-        0: "",
-        1: "C",
-        2: "CC",
-        3: "CCC",
-        4: "CD",
-        5: "D",
-        6: "DC",
-        7: "DCC",
-        8: "DCCC",
-        9: "CM"
-    };
-    const thousandthNumbers = {
-        0: "",
-        1: "M",
-        2: "MM",
-        3: "MMM"
-    };
-    let result;
-     let converted = Number(num);
-    if (typeof converted === 'number') {
-        converted = String(converted);
-        let itemPostition = 1;
-        for (let i = 0; i < converted.length; ++i) {
-            let num = converted[converted.length - itemPostition];
-            if (itemPostition === 1) {
-                result = singleNumber[num];
-            } else if (itemPostition === 2) {
-                result = decimalNumbers[num] + result;
-            } else if (itemPostition === 3) {
-                result = hundrethsNumbers[num] + result;
-            } else if (itemPostition === 4) {
-                result = thousandthNumbers[num] + result;
-            };
-            ++itemPostition;
-        };
-    } else {
-        console.log(false);
-    };
-    
-    return result;
+    let finalSum = '';
+    const reverseDict = Object.keys(romanSigns).reverse();
 
+    const getRomanSigns = (index, num) => {
+        const startIndex = reverseDict.indexOf(index);
+        for (let i = startIndex; i <= 12 ; ++i) {
+            const sum = num - reverseDict[i];
+            if (sum >= 0) {
+                finalSum = finalSum + romanSigns[reverseDict[i]];
+                num = sum
+                break;
+            };
+        };
+        return handleIntToRoman(num)
+    };
+    const handleIntToRoman = (num) => {
+        if (num === 0) {
+            return finalSum;
+        }
+        else if (num <= 3999) {
+        return getRomanSigns('1000', num);
+        } 
+        else if (num <= 99) {
+        return getRomanSigns('90', num);
+        } 
+        else if (num <= 9) {
+        return getRomanSigns('9', num);
+        };
+        if (num > 3999) {
+            return '';
+        };
+    };
+return handleIntToRoman(num);
 };
+
+
+
+// ===============================================================================================
+
+
+
 
 /*
 4.https://leetcode.com/problems/longest-substring-without-repeating-characters/
 */
+// РЕШЕНИЕ
 
-// РЕШЕНИЕ (НЕ решена доконца, так-как не прошла последний тест leetCode, "Превышен лимит времени")
 var lengthOfLongestSubstring = function(s) {
-    if (!(s.length === 0) && !(isNaN(s) === 'number')) {
-        
-        const repSubstr = [];
-        let substr;
-        let positionStart = 0;
-        let positionEnd = 1;
+    const substrings = new Set();
+    // convert character "\" to text
+    s = s.replace(/[\n\r\t]/g, match => 
+    match === '\n' ? '\\n' : match === '\r' ? '\\r' : '\\t');
 
-        for (;positionEnd <= s.length;) {
-            substr = s.slice(positionStart, positionEnd);
-            if (!(repSubstr.includes(substr))) {
-                const symbols = [];
-                for (let checkSymbol = 1; checkSymbol <= substr.length; ++checkSymbol ) {
-                    const lastSymbol = 1;
-                    if (substr[substr.length - checkSymbol] === substr[substr.length - lastSymbol]) {
-                        symbols.push(substr[substr.length - checkSymbol]);
+    const addSubstrings = (string) => {
+        // function to check first and last character for duplicates
+        const searchRepeatingSymbol = (substr) => {
+            const firstSymbol = substr[0];
+            const lastSymbol = substr[substr.length - 1];
+
+            if (firstSymbol === lastSymbol) {
+                return true;
+            }
+            
+            const trimmedSubstr = substr.slice(1, -1);
+            return trimmedSubstr.includes(firstSymbol) || trimmedSubstr.includes(lastSymbol);
+        };
+        // calculating and adding a substring
+        let endPosition = 1; 
+        for (let i = 0; i < string.length; ++i) {
+            for (let ii = endPosition; ii <= string.length; ++ii) { 
+                const substr = string.slice(i, ii);
+                if (substr.length > 1) {
+                    if (searchRepeatingSymbol(substr)) {
+                        endPosition = ii; 
+                        break;
+                    } else if (!substrings.has(substr)) {   
+                        substrings.add(substr);                
                     };
+                } else if (!substrings.has(substr)) {
+                    substrings.add(substr); 
                 };
-                if (symbols.length > 1) {
-                    positionEnd = ++positionStart;
-                    ++positionEnd;
-                } else {
-                    repSubstr.push(substr);
-                };
-            } else if (repSubstr.includes(substr)) {
-                ++positionEnd;
             };
         };
-        function findLongString(repSubstr) {
-            let itemOne = repSubstr[0];
-            for (let itemtwo of repSubstr) {
-                if (itemOne.length < itemtwo.length) {
-                    itemOne = itemtwo;
-                }; 
-            };
-            return itemOne.length;
-        };
-        return findLongString(repSubstr);
-    } else {
-        return 0;
+        
     };
+    addSubstrings(s);
+    // find substring of maximum length
+    let maxLength = 0;
+    for (const substr of substrings) {
+        maxLength = Math.max(maxLength, substr.length)
+    }
+    return maxLength;
 };
